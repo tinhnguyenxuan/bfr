19d38a4743873fb4300979256bb643095c35b353
Nguyễn Xuân Tĩnh <101899547+tinhnguyenxuan@users.noreply.github.com>
2022-08-21 19:16:41 +0700

    Update myJs.js
    Signed-off-by: Nguyễn Xuân Tĩnh <101899547+tinhnguyenxuan@users.noreply.github.com>

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://me.momo.vn/4GIEuasJuVFqFgCdCjTQ/y1aK86jr0oplbQG";
+            window.location = "https://me.momo.vn/4GIEuasJuVFqFgCdCjTQ/lNbWP8ZmDwP4dyg";
           },
         });
       }
